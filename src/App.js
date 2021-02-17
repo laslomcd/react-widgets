6794e3845516d42d5890257c71ad0c68566e070f
ryan <laslomcd@gmail.com>
2021-02-17 16:59:52 -0500

    dropdown cleanup useEffect

--- src/App.js
+++ src/App.js
@@ -35,10 +35,12 @@ const options = [
 
 const App = () => {
 	const [selected, setSelected] = useState(options[0]);
+	const [showDropdown, setShowDropdown] = useState(true);
 
 	return (
 		<div>
-			<Dropdown options={options} selected={selected} onSelectedChange={setSelected} />
+			<button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
+			{showDropdown ? <Dropdown options={options} selected={selected} onSelectedChange={setSelected} /> : null}
 		</div>
 	);
 };
