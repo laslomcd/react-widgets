76406a08f2e302e0f7c27a0c7b3e8a65bfb1cf69
ryan <laslomcd@gmail.com>
2021-02-23 17:06:30 -0500

    started translate component

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,24 +1,24 @@
 import React, { useState, useEffect, useRef } from "react";
 
-const Dropdown = ({ options, selected, onSelectedChange }) => {
+const Dropdown = ({ options, selected, onSelectedChange, label }) => {
 	const [open, setOpen] = useState(false);
 	const ref = useRef();
 
 	useEffect(() => {
 		const onBodyClick = (event) => {
-			if (!ref || ref.current === null) {
+			if (ref.current === null) {
 				return;
 			}
 			if (ref.current.contains(event.target)) {
 				return;
 			}
-
 			setOpen(false);
 		};
 
 		document.body.addEventListener("click", onBodyClick);
 
 		return () => {
+			console.log("removed");
 			document.body.removeEventListener("click", onBodyClick);
 		};
 	}, []);
@@ -38,7 +38,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
 	return (
 		<div ref={ref} className="ui form">
 			<div className="field">
-				<label className="label">Select a Color</label>
+				<label className="label">{label}</label>
 				<div onClick={() => setOpen(!open)} className={`ui selection dropdown ${open ? "visible active" : ""}`}>
 					<i className="dropdown icon"></i>
 					<div className="text">{selected.label}</div>
